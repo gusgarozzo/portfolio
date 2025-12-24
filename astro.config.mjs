d3eac5e335eac76afbd4318741cc5a60b0492363
gustavo.garozzo <gustavo.garozzo@aiotek.com>
2025-12-24 10:56:04 -0300

    feat: Add SEO component, sitemap integration, and robots.txt, and update hero section content.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,15 @@
 import { defineConfig } from "astro/config";
 import path from "path";
+import sitemap from "@astrojs/sitemap";
 
 export default defineConfig({
+  site: "https://gustavo-garozzo.vercel.app/", // Change this to your production domain
+  integrations: [sitemap()],
   vite: {
     resolve: {
       alias: {
-        "@": path.resolve("./src")
-      }
-    }
-  }
+        "@": path.resolve("./src"),
+      },
+    },
+  },
 });
